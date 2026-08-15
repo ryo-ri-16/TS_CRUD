0b52f6886cdffe7a29aa045eb58e16466acd8cda
ryo-ri-16 <ujiro1998@gmail.com>
2026-08-15 20:22:35 +0900

    Create機能

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import users from './routes/users'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.route('/users', users)
+
+serve({
+  fetch: app.fetch,
+  port: 3000,
 })
 
-export default app
+console.log('Server running on http://localhost:3000')
--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,11 +1,12 @@
-import { PrismaClient } from '../generated/prisma'
+import "dotenv/config"
+import { PrismaClient } from "../generated/prisma"
+import { PrismaPg } from "@prisma/adapter-pg"
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-  })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma = new PrismaClient({
+  adapter,
+  log: ["query", "error", "warn"],
+})
